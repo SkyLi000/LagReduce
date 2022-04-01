a9e298c4ab275e4dbb22802ce95ff63dee01184c
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-01 13:32:54 +1300

    Update PetSimXOverNightHatchLagReduce.lua

--- PetSimXOverNightHatchLagReduce.lua
+++ PetSimXOverNightHatchLagReduce.lua
@@ -1,14 +1,13 @@
-while wait() do
+while wait(getgenv().Settings.WaitTimeUntilNextCheck + getgenv().Settings.WaitTimeUntilInventoryClose) do
     if getgenv().Settings.Enabled then
         local Notify = getsenv(game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("Admin Commands"):WaitForChild("Admin Cmds Client")).AddNotification
         local Count = game.Players.LocalPlayer.PlayerGui.Main.Bottom.Inventory.Notification.Count
         if tonumber(Count.Text) >= getgenv().Settings.MaxiumumNotificationCount then
             Notify("Counter Reached "..getgenv().Settings.MaxiumumNotificationCount.." | Opening Inventory")
             game.Players.LocalPlayer.PlayerGui.Inventory.Enabled = true
-            wait(getgenv().Settings.WaitTime_Inv)
+            wait(getgenv().Settings.WaitTimeUntilInventoryClose)
             Notify("Waited: "..getgenv().Settings.WaitTimeUntilInventoryClose.."(s) | Closing Inventory")
             game.Players.LocalPlayer.PlayerGui.Inventory.Enabled = false
-            wait(getgenv().Settings.Wait_Time_Check)
             Notify("Waited: "..getgenv().Settings.WaitTimeUntilNextCheck)
         end
     end
